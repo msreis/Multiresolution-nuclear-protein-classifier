f98cc805a39fa4201314169ccf70c99924f45120
Ciro Oliveira <ciro.oliveira@semantix.com.br>
2019-08-14 14:22:31 -0300

    updating swissprot parsing

--- src/swissprot_analysis.R
+++ src/swissprot_analysis.R
@@ -1,6 +1,8 @@
-require(XML)
+library(XML)
 
-wd = '/home/cirofdo/Documents/Multiresolution-nuclear-protein-classifier/'
+#wd <- '/home/cirofdo/Documents/Multiresolution-nuclear-protein-classifier/'
+wd <- 'C:/Users/cirof/Documents/Multiresolution-nuclear-protein-classifier-organize-data/'
+
 setwd(wd)
 
 data <- xmlParse('output/swiss-prot--T-cruzi.blastp.xml')
@@ -12,12 +14,14 @@ iterations <- as.list(xml_data$BlastOutput_iterations)
 df_swiss_prot_t_cruzi <- data.frame()
 
 for(i in 1:length(iterations)){
-  df_swiss_prot_t_cruzi[i, 'query_id'] <- iterations[i]$Iteration$`Iteration_query-def`
+  df_swiss_prot_t_cruzi[i, 'iteration_query_def'] <- iterations[i]$Iteration$`Iteration_query-def`
   df_swiss_prot_t_cruzi[i, 'aln_length'] <- as.integer(iterations[i]$Iteration$`Iteration_query-len`)
   df_swiss_prot_t_cruzi[i, 'flag_hit'] <- ifelse(iterations[i]$Iteration$Iteration_hits=="\n", 0, length(iterations[i]$Iteration$Iteration_hits$Hit$Hit_hsps))
-  #df_swiss_prot_t_cruzi[i, 'subject_id'] <- iterations[i]$Iteration$Iteration_hits$`Hit`$Hit_def
-  #df_swiss_prot_t_cruzi[i, 'e_value'] <- iterations[i]$Iteration$Iteration_hits$`Hit`$Hit_hsps$`Hsp`$Hsp_evalue
-  #df_swiss_prot_t_cruzi[i, 'bit_score'] <- iterations[i]$Iteration$Iteration_hits$`Hit`$Hit_hsps$`Hsp`$`Hsp_bit-score`
+  if(df_swiss_prot_t_cruzi[i, 'flag_hit']>=1) {
+    df_swiss_prot_t_cruzi[i, 'query_id'] <- iterations[i]$Iteration$Iteration_hits$`Hit`$Hit_def
+    #df_swiss_prot_t_cruzi[i, 'e_value'] <- iterations[i]$Iteration$Iteration_hits$`Hit`$Hit_hsps$`Hsp`$Hsp_evalue
+    #df_swiss_prot_t_cruzi[i, 'bit_score'] <- iterations[i]$Iteration$Iteration_hits$`Hit`$Hit_hsps$`Hsp`$`Hsp_bit-score`
+  }
 }
 
 
