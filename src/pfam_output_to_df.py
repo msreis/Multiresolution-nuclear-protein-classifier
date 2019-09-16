0eff9fa3431a40391cda0685d738202cd8cefba8
cirofdo <cirooliveira3@gmail.com>
2019-09-15 23:55:22 -0300

    fix code to correctly read score column

--- src/pfam_output_to_df.py
+++ src/pfam_output_to_df.py
@@ -13,7 +13,7 @@ lines = [line.rstrip('\n') for line in open(wd + 'output/output-file-tbl')]
 
 for i in range(len(lines)-13):
     df.loc[i] = [lines[i+3][:21],lines[i+3][21:32],lines[i+3][32:51],lines[i+3][51:65],
-                 lines[i+3][65:75],lines[i+3][75:82],lines[i+3][82:89],lines[i+3][89:98],
+                 lines[i+3][65:75],lines[i+3][75:80],lines[i+3][82:89],lines[i+3][89:98],
                  lines[i+3][98:105],lines[i+3][105:112],lines[i+3][112:118],lines[i+3][118:122],
                  lines[i+3][122:126],lines[i+3][126:130],lines[i+3][130:134],lines[i+3][134:138],
                  lines[i+3][138:142],lines[i+3][142:144],lines[i+3][-(len(lines[i+3])-144):]]
