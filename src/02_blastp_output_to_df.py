106e7f9960726d3711b87bd164a0beee45378da2
cirofdo <ciro.oliveira@semantix.com.br>
2019-11-24 11:32:30 -0300

    update scripts

--- src/02_blastp_output_to_df.py
+++ src/02_blastp_output_to_df.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 wd = '/home/cirofdo/Documents/Multiresolution-nuclear-protein-classifier/'
 
-f = open(wd + 'output/blastn_header.txt', 'r')
-col_names = f.read().split()
+col_names = ['query_id','subject_id','pct_identity','aln_length','n_of_mismatches','gap_openings',
+			 'q_start','q_end','s_start','s_end','e_value','bit_score']
 
 df = pd.read_csv(wd + 'output/swiss-prot--T-cruzi.blastp', sep='	', names=col_names)
 df.query_id = df.query_id.apply(lambda x: x.replace('sp|', '')[:6])
